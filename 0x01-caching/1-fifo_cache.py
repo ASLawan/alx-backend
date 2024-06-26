6e93eab5e79a33b021dafc1871ca9ad1ec4c5cbc
ASLawan <nawalnitsua@gmail.com>
2024-06-26 02:21:26 +0100

    Fix code output errors

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -22,7 +22,7 @@ class FIFOCache(BaseCaching):
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                     first_key = self.order.pop(0)
                     del self.cache_data[first_key]
-                    print("DISCARDED: {}".format(first_key))
+                    print("DISCARD: {}".format(first_key))
                 self.order.append(key)
             self.cache_data[key] = item
 
