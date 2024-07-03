bfa0968e6dab898a6c2850b93d0ecef396045ac2
ASLawan <nawalnitsua@gmail.com>
2024-07-03 18:28:16 +0100

    Fix code output errors

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,7 +22,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Determines the best match from supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
