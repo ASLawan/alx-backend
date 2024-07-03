64deab7054d5731c6bfd766bd89467f6c8e08a07
ASLawan <nawalnitsua@gmail.com>
2024-07-03 14:11:32 +0100

    Parametize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+    Module implmenting a basic flask app with
+    i18n support
+
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Class defining i18n default values"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determines the best match from supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """default app route"""
+    home_title = _("home_title")
+    home_header = _("home_header")
+    return render_template('3-index.html', home_title=home_title, home_header=home_header)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
